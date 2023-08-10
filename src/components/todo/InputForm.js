288975134bce225480dbbfe59e8190fc77194b20
Ely Dorado <2018dorado@gmail.com>
2023-08-10 16:33:51 -0500

    todo list components developing

---
+++ src/components/todo/InputForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function InputForm() {
+  return (
+    <div>InputForm</div>
+  )
+}
+
+/*
+Create an input form where users can type in their task and submit it. 
+Use the onChange event to update the input value in the state as the user types.
+*/
