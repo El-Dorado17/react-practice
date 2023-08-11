492f6384ce861c1930695b7ee2362fbbb3c1ad6c
Ely Dorado <2018dorado@gmail.com>
2023-08-11 15:52:24 -0500

    todo in progress; still needs a delete/complete task

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import React, {useState} from "react";
-import './App.css';
 
-function App () {
-  const [tasks, setTasks] = useState([])
-  const [newTask, setNewTask] = useState('')
+//! TODO LIST CODE FOR PRACTICE
+// import React, {useState} from "react";
+// import './App.css';
 
-  const addTask = () =>{
-    if (newTask.trim() !==''){
-      setTasks([...tasks, {id: Date.now(), text: newTask, completed: false}])
-      setNewTask('')
-    }
-  }
+// function App () {
+//   const [tasks, setTasks] = useState([])
+//   const [newTask, setNewTask] = useState('')
 
-  const toggleTask = (id) =>{
-    const updatedTasks = tasks.map((task)=>
-    task.id === id ? { ...task, completed: !task.completed} : task
-    )
+//   const addTask = () =>{
+//     if (newTask.trim() !==''){
+//       setTasks([...tasks, {id: Date.now(), text: newTask, completed: false}])
+//       setNewTask('')
+//     }
+//   }
 
-    setTasks(updatedTasks)
-  }
+//   const toggleTask = (id) =>{
+//     const updatedTasks = tasks.map((task)=>
+//     task.id === id ? { ...task, completed: !task.completed} : task
+//     )
 
-  return(
-    <div className="App">
-      <h1>To-Do list</h1>
-      <div>
-        <input
-          type = 'text'
-          value= {newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          placeholder="Add something you need to do"
-          />
-          <button onClick={addTask}>Add</button>
-      </div>
-      <ul>
-        {tasks.map((task) => (
-          <li
-            key={task.id}
-            className={task.completed ? 'completed' : ''}
-            onClick={() => toggleTask(task.id)}
-          >
-            {task.text}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+//     setTasks(updatedTasks)
+//   }
 
-export default App;
+//   return(
+//     <div className="App">
+//       <h1>To-Do list</h1>
+//       <div>
+//         <input
+//           type = 'text'
+//           value= {newTask}
+//           onChange={(e) => setNewTask(e.target.value)}
+//           placeholder="Add something you need to do"
+//           />
+//           <button onClick={addTask}>Add</button>
+//       </div>
+//       <ul>
+//         {tasks.map((task) => (
+//           <li
+//             key={task.id}
+//             className={task.completed ? 'completed' : ''}
+//             onClick={() => toggleTask(task.id)}
+//           >
+//             {task.text}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App;
