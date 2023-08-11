19e52245606fb15caeaf4501ba916fdd80d8722e
Ely Dorado <2018dorado@gmail.com>
2023-08-11 15:50:35 -0500

    made a simple todo function to mess with useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-//import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
-import { Random } from './components/random/Random';
-//import Greet from './components/Greet'; //to see why this is named TACO, see Greet.js
-//import Welcome from './components/Welcome';
-//import Hello from './components/Hello';
-// import Message from './components/Message';
-// import Counter from './components/Counter';
-// import FunctionClick from './components/FunctionClick';
-// import ClassClick from './components/ClassClick';
-// import EventBind from './components/EventBind';
 
+function App () {
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
 
-function App() {
-  return (
+  const addTask = () =>{
+    if (newTask.trim() !==''){
+      setTasks([...tasks, {id: Date.now(), text: newTask, completed: false}])
+      setNewTask('')
+    }
+  }
+
+  const toggleTask = (id) =>{
+    const updatedTasks = tasks.map((task)=>
+    task.id === id ? { ...task, completed: !task.completed} : task
+    )
+
+    setTasks(updatedTasks)
+  }
+
+  return(
     <div className="App">
-      <Random/>
+      <h1>To-Do list</h1>
+      <div>
+        <input
+          type = 'text'
+          value= {newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add something you need to do"
+          />
+          <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            className={task.completed ? 'completed' : ''}
+            onClick={() => toggleTask(task.id)}
+          >
+            {task.text}
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
