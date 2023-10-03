8669877e2f01cf235435840aaab19e112d62e7e2
Ely Dorado <2018dorado@gmail.com>
2023-10-03 13:54:07 -0500

    practice

--- src/components/coffee/savings.js
+++ src/components/coffee/savings.js
@@ -1,14 +1,14 @@
-import React from "react";
+// import React from "react";
 
-export const Savings = (savings ) =>{
-    const monthly = savings * 30
-    const yearly = savings * 365
+// export const Savings = (savings ) =>{
+//     const monthly = savings * 30
+//     const yearly = savings * 365
 
-    return (
-        <>
-            <div>
-                <p></p>
-            </div>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <div>
+//                 <p></p>
+//             </div>
+//         </>
+//     )
+// }
